e7799a59b1dae432a7c6c7ddad5e76941a6c8af0
Teodora Slavova <tedi.slava@yahoo.com>
2023-10-19 20:50:37 +0300

    starter code for web gui
    setting up service on port 5500 and welcome page w/out css

---
+++ web_interface/index.js
@@ -0,0 +1,29 @@
+    const express = require("express");
+    const app = express();
+    const port = 5500; 
+    const expressHandlebars = require("express-handlebars");
+
+    app.engine(".hbs", expressHandlebars.create({
+        extname: ".hbs"
+    }).engine);
+    app.set( "view engine" , ".hbs");
+    app.use('/static', express.static('static'));
+    app.use(express.urlencoded({extended: true}));
+    app.set('views', './views');
+
+    app.route("/")
+    .get((req, res) => {
+       res.render("homePage", (err, html) => {
+           if(err) {
+               res.statusCode = 444; 
+               console.log(err);
+                res.send("Something went wrong!");
+                
+           } else {
+                res.send(html);
+           }
+       })
+    })
+
+
+    app.listen(port, () => console.log(`App listening on: ${port}`));
