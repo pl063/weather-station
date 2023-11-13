167c9da452c3ab40963c8de55861adc30133d0b1
Teodora Slavova <tedi.slava@yahoo.com>
2023-11-13 22:14:48 +0200

    added log page to web gui

--- web_interface/index.js
+++ web_interface/index.js
@@ -4,12 +4,18 @@
     const expressHandlebars = require("express-handlebars");
 
     app.engine(".hbs", expressHandlebars.create({
-        extname: ".hbs"
+        extname: ".hbs",
+        defaultLayout: 'main', 
+        layoutsDir: __dirname + '/views/layouts/',
+        partialsDir: __dirname + '/views/partials/'
     }).engine);
     app.set( "view engine" , ".hbs");
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({extended: true}));
     app.set('views', './views/');
+
+    app.enable("view cache");
+    process.env.NODE_ENV === "production";
 
     app.route("/").get((req, res) => { res.redirect('/dashboard')
         res.statusCode = 301;
@@ -17,16 +23,30 @@
 
     app.route("/dashboard")
     .get((req, res) => {
-       res.render("homePage", (err, html) => {
-           if(err) {
-               res.statusCode = 444; 
-               console.log(err);
-                res.send("Something went wrong!");
-                
-           } else {
-                res.send(html);
-           }
-       })
+        res.render("homePage", {layout: "main"}, (err, html) => {
+            if(err) {
+                res.statusCode = 444; 
+                console.log(err);
+                 res.send("Something went wrong!");
+                 
+            } else {
+                 res.send(html);
+            }
+        });
+    })
+
+    app.route("/logs")
+    .get( (req, res) => {
+        res.render("homePage", {layout: "logs"}, (err, html) => {
+            if(err) {
+                res.statusCode = 444; 
+                console.log(err);
+                 res.send("Something went wrong!");
+                 
+            } else {
+                 res.send(html);
+            }
+        });
     })
 
 
