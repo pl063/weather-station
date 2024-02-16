7b0dc934b154c0d4cf8d49922f8484a92192de81
Teodora Slavova <tedi.slava@yahoo.com>
2024-02-16 10:30:00 +0200

    Update main_weather.py
    Add comments

--- reading_sensors/main_weather.py
+++ reading_sensors/main_weather.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-# Import libraries:
-#raindrop sensor DO connected to GPIO18
+# Raindrop sensor IS connected to GPIO18
+# Bme280 sensor IS connected to SDA, SCL, GPIO17 (+) and GND
 
 from time import sleep
 import datetime
@@ -21,7 +21,7 @@ from led_output import led_output
 weather_arr = []
 timer = 60 #sleep timer for main loop in seconds
 average_counter = 10 #how many objects should be averaged
-flags = [False]
+flags = [False] #check if the collection is already migrated
 
 
 class Current_state_class : 
@@ -36,31 +36,32 @@ def main():
     currentTime = datetime.datetime.now()
     flag = flags[len(flags) - 1]
     logging.info("The current flag is " + str(flag))
-    if (currentTime.hour == 0 and flag == False): 
+    if (currentTime.hour == 0 and flag == False): #check if the current day is over and if the collection is already migrated
         #it is midnight, perform migration
         logging.info("Callig mainMigration function")
         mainMigration()
         flags.append(True)
         
     logging.info("Main weather is running " + extractTime())
-    weatherList = extractBME()
-    rainFlag = determineRainState()
+    weatherList = extractBME() #extract info from the bme sensor
+    rainFlag = determineRainState() #extract info from the rain sensor
     #write values in current object
-    current_state = Current_state_class(weatherList[0], weatherList[1], weatherList[2], rainFlag)
+    current_state = Current_state_class(weatherList[0], weatherList[1], weatherList[2], rainFlag) #generate current state
     #Print object in readable format
     #res = vars(current_state)
     #pprint(res)
-    #add state to the array
+     
+    #add state to the cache array
     weather_arr.append(current_state)
 
     if(len(weather_arr) == average_counter):
           logging.info("Averagazing 10 states" + extractTime())
           try :
-              t = average_states(weather_arr, average_counter)
+              t = average_states(weather_arr, average_counter) #averagize the states
               weather_arr.clear()
               #pprint(vars(t))
               logging.info("Averagized done " + extractTime())
-              insert_current_state(t.__dict__)
+              insert_current_state(t.__dict__) #send to the database
               logging.info("api finished " + extractTime())
           except Exception as arg:
               logging.error(extractTime(), arg)
